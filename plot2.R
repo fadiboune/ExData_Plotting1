0fa532449060926056e1999b08c440cdcea6fb96
Fabrice Collin <fabricecollin31@gmail.com>
2016-03-10 21:53:11 +0100

    second commit for second plot

--- plot2.R
+++ plot2.R
@@ -1,8 +1,9 @@
 # ***********************************************************************
-# ExData_Plotting1 - code for plot 1
+# ExData_Plotting1 - code for plot 2
 # ***********************************************************************
 # work with the file "household_power_consumption.txt" (in your working directory)
-# 
+#
+library(lubridate)
 #
 # reading subset of data (1/2/2007-2/2/2007)
 selDate<- grep("^(1|2)/2/2007", readLines("household_power_consumption.txt"))
@@ -17,11 +18,16 @@ hhenergy<- read.table("household_power_consumption.txt",
                       sep=";",
                       stringsAsFactors = FALSE)
 #
+# convert date and time, paste in first column, delete Time column
+hhenergy$Date<- dmy_hms(paste(hhenergy$Date, hhenergy$Time))
+hhenergy$Time<- NULL
+#
 # plotting
-with(hhenergy, hist(Global_active_power,
-                    col="red",
-                    xlab="Global Active Power (kW)",
-                    main="Global Active Power"))
+with(hhenergy, plot(Date, Global_active_power,
+                    type="l",
+                    xlab="",
+                    ylab="Global Active Power (kW)"))
+#
 # writing to png format
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
